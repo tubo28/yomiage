498481d4731fd193560c7ca41a02eecfb44a1489
tubo28 <arsenic28@gmail.com>
2021-01-02 01:53:53 +0900

    [feat] play pong in reply to ping message

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.13
 require (
 	cloud.google.com/go v0.74.0 // indirect
 	github.com/bwmarrin/discordgo v0.22.0 // indirect
+	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
 	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
+	layeh.com/gopus v0.0.0-20161224163843-0ebf989153aa // indirect
 )
--- main2.go
+++ main2.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"encoding/binary"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -15,13 +14,20 @@ import (
 
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	"github.com/bwmarrin/discordgo"
+	"github.com/jonas747/ogg"
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
 var (
 	token  = "Nzk0NDkzODg2ODI2ODA3MzI2.X-7oFw.Z2HjIydKzkCJ9B6PpsC-xxj-1RE"
-	buffer = make([][]byte, 0)
 	client *texttospeech.Client
+)
+
+const (
+	channels  int = 2                   // 1 for mono, 2 for stereo
+	frameRate int = 48000               // audio sampling rate
+	frameSize int = 960                 // uint16 size of each audio frame
+	maxBytes  int = (frameSize * 2) * 2 // max size of opus data
 )
 
 func main() {
@@ -33,13 +39,6 @@ func main() {
 	client, err = texttospeech.NewClient(context.TODO())
 	if err != nil {
 
-	}
-
-	// Load the sound file.
-	if err := loadSound(); err != nil {
-		fmt.Println("Error loading sound: ", err)
-		fmt.Println("Please copy $GOPATH/src/github.com/bwmarrin/examples/airhorn/airhorn.dca to this directory.")
-		return
 	}
 
 	// Create a new Discord session using the provided bot token.
@@ -85,36 +84,6 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// check if the message is "!airhorn"
-	if strings.HasPrefix(m.Content, "!airhorn") {
-
-		// Find the channel that the message came from.
-		c, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			// Could not find channel.
-			return
-		}
-
-		// Find the guild for that channel.
-		g, err := s.State.Guild(c.GuildID)
-		if err != nil {
-			// Could not find guild.
-			return
-		}
-
-		// Look for the message sender in that guild's current voice states.
-		for _, vs := range g.VoiceStates {
-			if vs.UserID == m.Author.ID {
-				err = playSound(s, g.ID, vs.ChannelID, buffer)
-				if err != nil {
-					fmt.Println("Error playing sound:", err)
-				}
-
-				return
-			}
-		}
-	}
-
 	if strings.HasPrefix(m.Content, "!ping") {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 
@@ -140,72 +109,51 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			log.Fatal(err)
 		}
 
-		filename := "output.ogg"
-		err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+		log.Printf("ogg data created %d bytes", len(resp.AudioContent))
+		oggData := makeBuffer(resp.AudioContent)
+
+		// Find the channel that the message came from.
+		c, err := s.State.Channel(m.ChannelID)
 		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Audio content written to file: %v\n", filename)
-	}
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// guild is joined.
-func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-
-	if event.Guild.Unavailable {
-		return
-	}
-
-	for _, channel := range event.Guild.Channels {
-		if channel.ID == event.Guild.ID {
-			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			// Could not find channel.
 			return
 		}
+
+		// Find the guild for that channel.
+		g, err := s.State.Guild(c.GuildID)
+		if err != nil {
+			// Could not find guild.
+			return
+		}
+
+		// Look for the message sender in that guild's current voice states.
+		for _, vs := range g.VoiceStates {
+			if vs.UserID == m.Author.ID {
+				err = playSound(s, g.ID, vs.ChannelID, oggData)
+				if err != nil {
+					fmt.Println("Error playing sound:", err)
+				}
+				return
+			}
+		}
 	}
 }
 
-// loadSound attempts to load an encoded sound file from disk.
-func loadSound() error {
+func makeBuffer(in []byte) (output [][]byte) {
+	// Setup our ogg and packet decoders
+	oggdecoder := ogg.NewDecoder(bytes.NewReader(in))
+	packetdecoder := ogg.NewPacketDecoder(oggdecoder)
 
-	file, err := os.Open("airhorn.dca")
-	if err != nil {
-		fmt.Println("Error opening dca file :", err)
-		return err
-	}
-
-	var opuslen int16
-
+	// Run through the packet decoder appending the bytes to our output [][]byte
 	for {
-		// Read opus frame length from dca file.
-		err = binary.Read(file, binary.LittleEndian, &opuslen)
-
-		// If this is the end of the file, just return.
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			err := file.Close()
-			if err != nil {
-				return err
+		packet, _, err := packetdecoder.Decode()
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
 			}
-			return nil
+			return output
 		}
-
-		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
-			return err
-		}
-
-		// Read encoded pcm from dca file.
-		InBuf := make([]byte, opuslen)
-		err = binary.Read(file, binary.LittleEndian, &InBuf)
-
-		// Should not be any end of file errors
-		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
-			return err
-		}
-
-		// Append encoded pcm data to the buffer.
-		buffer = append(buffer, InBuf)
+		output = append(output, packet)
 	}
 }
 
@@ -239,4 +187,20 @@ func playSound(s *discordgo.Session, guildID, channelID string, buffer [][]byte)
 	vc.Disconnect()
 
 	return nil
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// guild is joined.
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+
+	if event.Guild.Unavailable {
+		return
+	}
+
+	for _, channel := range event.Guild.Channels {
+		if channel.ID == event.Guild.ID {
+			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			return
+		}
+	}
 }
