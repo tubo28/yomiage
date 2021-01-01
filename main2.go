71f680f0110a144112b544fbae81f4ab64a0bb82
tubo28 <arsenic28@gmail.com>
2021-01-01 21:34:34 +0900

    [feat] use tts service and save created ogg

--- main2.go
+++ main2.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -11,12 +13,15 @@ import (
 	"syscall"
 	"time"
 
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	"github.com/bwmarrin/discordgo"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
 var (
 	token  = "Nzk0NDkzODg2ODI2ODA3MzI2.X-7oFw.Z2HjIydKzkCJ9B6PpsC-xxj-1RE"
 	buffer = make([][]byte, 0)
+	client *texttospeech.Client
 )
 
 func main() {
@@ -24,9 +29,14 @@ func main() {
 		log.Fatal("no discord token is given")
 	}
 
-	// Load the sound file.
-	err := loadSound()
+	var err error
+	client, err = texttospeech.NewClient(context.TODO())
 	if err != nil {
+
+	}
+
+	// Load the sound file.
+	if err := loadSound(); err != nil {
 		fmt.Println("Error loading sound: ", err)
 		fmt.Println("Please copy $GOPATH/src/github.com/bwmarrin/examples/airhorn/airhorn.dca to this directory.")
 		return
@@ -95,7 +105,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		// Look for the message sender in that guild's current voice states.
 		for _, vs := range g.VoiceStates {
 			if vs.UserID == m.Author.ID {
-				err = playSound(s, g.ID, vs.ChannelID)
+				err = playSound(s, g.ID, vs.ChannelID, buffer)
 				if err != nil {
 					fmt.Println("Error playing sound:", err)
 				}
@@ -103,6 +113,39 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				return
 			}
 		}
+	}
+
+	if strings.HasPrefix(m.Content, "!ping") {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+
+		req := texttospeechpb.SynthesizeSpeechRequest{
+			// Set the text input to be synthesized.
+			Input: &texttospeechpb.SynthesisInput{
+				InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Pong!"},
+			},
+			// Build the voice request, select the language code ("en-US") and the SSML
+			// voice gender ("neutral").
+			Voice: &texttospeechpb.VoiceSelectionParams{
+				LanguageCode: "en-US",
+				SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+			},
+			// Select the type of audio file you want returned.
+			AudioConfig: &texttospeechpb.AudioConfig{
+				AudioEncoding: texttospeechpb.AudioEncoding_OGG_OPUS,
+			},
+		}
+
+		resp, err := client.SynthesizeSpeech(context.TODO(), &req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		filename := "output.ogg"
+		err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Audio content written to file: %v\n", filename)
 	}
 }
 
@@ -167,7 +210,7 @@ func loadSound() error {
 }
 
 // playSound plays the current buffer to the provided channel.
-func playSound(s *discordgo.Session, guildID, channelID string) (err error) {
+func playSound(s *discordgo.Session, guildID, channelID string, buffer [][]byte) (err error) {
 
 	// Join the provided voice channel.
 	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
