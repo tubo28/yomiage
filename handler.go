cc15a47f4a98709536077960063c3392099c4af5
tubo28 <arsenic28@gmail.com>
2021-01-02 17:33:02 +0900

    [fix] stop zombie worker

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -10,57 +11,73 @@ import (
 func hiHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	botID := m.Author.ID
 	guildID := m.GuildID
-	userVs, err := voiceState(s, botID, guildID)
 
+	userVs, err := voiceState(s, botID, guildID)
 	if err != nil {
 		log.Printf("failed to get VoiceState of guild %s: %s", guildID, err.Error())
 		return
 	}
 	if userVs == nil {
 		log.Printf("member %s is not joining any voice channel", botID)
+		s.ChannelMessageSend(m.ChannelID, "cannot summon me without joining any voice channel")
 		return
 	}
 
-	if conn, ok := dg.VoiceConnections[m.GuildID]; ok {
+	startWorker(guildID)
+	time.Sleep(200 * time.Millisecond) // waiting for bot to join voice channel
+	joinVC(s, m.GuildID, userVs.ChannelID, m.ChannelID)
+}
+
+func byeHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	stopWorker(m.GuildID)
+	leaveVC(s, m.GuildID, m.ChannelID)
+}
+
+func nonCommandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	t := task{
+		guildID: m.GuildID,
+		text:    m.Content,
+	}
+	addTask(m.GuildID, t)
+}
+
+func joinVC(s *discordgo.Session, guildID, vcID, tcID string) {
+	if conn, ok := dg.VoiceConnections[guildID]; ok {
 		// todo: force move here?
-		log.Printf("bot %s is already joining voice channel %s guild %s", botID, conn.ChannelID, conn.GuildID)
-		if conn.ChannelID == userVs.ChannelID {
-			s.ChannelMessageSend(m.ChannelID, "already here")
+		if conn.ChannelID == vcID {
+			log.Printf("bot is already joining target voice channel %s guild %s", conn.ChannelID, conn.GuildID)
+			s.ChannelMessageSend(tcID, "already here")
 		} else {
-			s.ChannelMessageSend(m.ChannelID, "bot is already working on other channel")
+			log.Printf("bot is already joining other voice channel %s guild %s", conn.ChannelID, conn.GuildID)
+			s.ChannelMessageSend(tcID, "bot is already working on other channel")
 		}
 		return
 	}
 
-	if _, err := s.ChannelVoiceJoin(guildID, userVs.ChannelID, false, true); err != nil {
-		log.Printf("failed to join channel %s on guild %s", userVs.ChannelID, userVs.GuildID)
+	if _, err := s.ChannelVoiceJoin(guildID, vcID, false, true); err != nil {
+		log.Printf("failed to join channel %s on guild %s", vcID, guildID)
 		return
 	}
 
 	// todo: install errcheck
-	if _, err := s.ChannelMessageSend(m.ChannelID, "I read text here"); err != nil {
-		log.Printf("failed to send message to channel %s", m.ChannelID)
+	if _, err := s.ChannelMessageSend(tcID, "I read text here"); err != nil {
+		log.Printf("failed to send message to channel %s", tcID)
 	}
+
 }
 
-func byeHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	go func() {
-		for len(taskQueue) > 0 {
-			t := <-taskQueue
-			log.Panicf("task %s is discarded. remain task %d", t.text, len(taskQueue))
-		}
-	}()
-	conn, ok := dg.VoiceConnections[m.GuildID]
+func leaveVC(s *discordgo.Session, guildID, tcID string) {
+	conn, ok := dg.VoiceConnections[guildID]
 	if !ok {
 		log.Print("not joining your voice channel")
-		s.ChannelMessageSend(m.ChannelID, "not joining your voice channel")
+		s.ChannelMessageSend(tcID, "not joining your voice channel")
 		return
 	}
 	if err := conn.Disconnect(); err != nil {
-		log.Printf("error disconnect from voice channel %s", conn.ChannelID)
+		log.Printf("error disconnect from voice channel %s: %s", conn.ChannelID, err.Error())
 		return
 	}
-	s.ChannelMessageSend(m.ChannelID, "Bye")
+	s.ChannelMessageSend(tcID, "Bye")
 }
 
 func voiceState(s *discordgo.Session, userID, guildID string) (*discordgo.VoiceState, error) {
--- main.go
+++ main.go
@@ -40,8 +40,6 @@ func main() {
 		fmt.Println("Error opening Discord session: ", err)
 	}
 
-	go ttsTaskConsumer()
-
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Airhorn is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
@@ -52,16 +50,25 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
 	if strings.HasPrefix(m.Content, "!hi") {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 		hiHandler(s, m)
 	} else if strings.HasPrefix(m.Content, "!bye") {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 		byeHandler(s, m)
 	} else if strings.HasPrefix(m.Content, "!lang") {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 		// set users lang
 	} else if strings.HasPrefix(m.Content, "!rand") {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 		// randomize voice
 	} else if !strings.HasPrefix(m.Content, "!") {
 		nonCommandHandler(s, m)
@@ -73,46 +80,93 @@ type task struct {
 	text    string
 }
 
-const queueCap = 32
+type worker struct {
+	guildID string
+	done    chan bool
+	queue   chan task
+}
 
-var taskQueue = make(chan task, queueCap)
+// one worker per guild
+var ttsWorkers = make(map[string]*worker)
 
-func nonCommandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	t := task{
-		guildID: m.GuildID,
-		text:    m.Content,
+func startWorker(guildID string) {
+	w, ok := ttsWorkers[guildID]
+	if ok {
+		log.Print("worker is already running for guild ", guildID)
+		return
+	}
+	w = &worker{
+		guildID: guildID,
+		done:    make(chan bool),
+		queue:   make(chan task, 32),
+	}
+	ttsWorkers[guildID] = w
+
+	go func() {
+	L:
+		for {
+			select {
+			case <-w.done:
+				log.Print("worker is stopped")
+				break L
+			case t, ok := <-w.queue:
+				if !ok {
+					log.Print("task queue is empty")
+					break L
+				}
+				log.Print("process task: ", t.text)
+				if err := doTask(t); err != nil {
+					log.Print("error processing task: ", err.Error())
+				}
+			}
+		}
+	}()
+}
+
+func addTask(guildID string, t task) {
+	w, ok := ttsWorkers[guildID]
+	if !ok {
+		log.Printf("no worker for guild %s, task %s is discarded", guildID, t.text)
+		return
 	}
 	select {
-	case taskQueue <- t:
+	case w.queue <- t:
+		log.Print("task is added: ", t.text)
 	default:
-		fmt.Println("discarding message due to chanel is full:", m.Content)
+		fmt.Println("discarding message due to chanel is full:", t.text)
 	}
 }
 
-func ttsTaskConsumer() {
-	log.Print("start ttsTaskConsumer")
-	for {
+func stopWorker(guildID string) {
+	w, ok := ttsWorkers[guildID]
+	if !ok {
+		log.Print("no worker for guild ", guildID)
+		return
+	}
+	w.done <- true
+	close(w.queue)
+	for t := range w.queue {
+		log.Print("task is discorded because worker is stopped: ", t.text)
+	}
+	delete(ttsWorkers, guildID)
+}
+
+func doTask(t task) error {
+	defer func() {
 		time.Sleep(200 * time.Millisecond)
-		if t, ok := <-taskQueue; ok {
-			consumeOne(t)
-		}
-		println("ttsTaskConsumer: remain task", len(taskQueue))
-	}
-}
-
-func consumeOne(t task) {
+	}()
 	conn, ok := dg.VoiceConnections[t.guildID]
 	if !ok {
-		log.Printf("voice channel on guild %s is deleted", t.guildID)
-		return
+		return fmt.Errorf("voice channel on guild %s is deleted. maybe zombie worker", t.guildID)
 	}
 
 	oggBuf, err := ttsOGGGoogle(t.text, "ja-JP")
 	if err != nil {
 		log.Printf("failed to create tts audio: %s", err.Error())
-		return
+		return nil
 	}
 	playOGG(conn, oggBuf)
+	return nil
 }
 
 func playOGG(conn *discordgo.VoiceConnection, oggBuf [][]byte) {
