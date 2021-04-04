7ccddf3ff75bc85830bcf2d8e71d8056230e0705
tubo28 <arsenic28@gmail.com>
2021-04-04 19:52:31 +0900

    refactor: add loose coupling worker

--- worker/worker.go
+++ worker/worker.go
@@ -1,120 +1,71 @@
 package worker
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"sync"
-	"time"
-
-	"github.com/tubo28/yomiage/discord"
 )
 
-// TTSTask is tts task executed async
-type TTSTask struct {
-	GuildID    string
-	Text       string
-	Lang       string
-	VoiceToken string
+const taskQueueCapacity = 32
+
+// Task executed async
+type Task struct {
+	ID string
+	Do func() error
 }
 
-type worker struct {
-	guildID string
-	done    chan bool
-	queue   chan TTSTask
+func NewTask(ID string, Do func() error) *Task {
+	return &Task{ID: ID, Do: Do}
 }
 
-// one worker per guild
-var ttsWorkers sync.Map
-
-// StartWorker starts task consumer
-func StartWorker(guildID string) {
-	_, ok := ttsWorkers.Load(guildID)
-	if ok {
-		log.Print("worker is already running for guild ", guildID)
-		return
-	}
-	w := &worker{
-		guildID: guildID,
-		done:    make(chan bool),
-		queue:   make(chan TTSTask, 32),
-	}
-	ttsWorkers.Store(guildID, w)
-
-	go func() {
-	L:
-		for {
-			select {
-			case <-w.done:
-				log.Printf("stop worker. %d tasks remains", len(w.queue))
-				break L
-			case t, ok := <-w.queue:
-				if !ok {
-					log.Print("task queue is empty")
-					break L
-				}
-				log.Print("process task: ", t.Text)
-				if err := do(t); err != nil {
-					log.Print("error processing task: ", err.Error())
-				}
-			}
-		}
-	}()
+type Consumer struct {
+	ID    string
+	queue chan Task
 }
 
-// AddTask adds task to queue
-func AddTask(guildID string, t TTSTask) {
-	iw, ok := ttsWorkers.Load(guildID)
-	if !ok {
-		log.Printf("no worker for guild %s, task %s is discarded", guildID, t.Text)
-		return
-	}
-	w := iw.(*worker)
-	select {
-	case w.queue <- t:
-		log.Print("task is added: ", t.Text)
-	default:
-		fmt.Println("discarding message due to chanel is full:", t.Text)
+func NewConsumer(ID string) *Consumer {
+	return &Consumer{
+		ID:    ID,
+		queue: make(chan Task, taskQueueCapacity),
 	}
 }
 
-// StopWorker stops worker for the guild specified by guildID
-func StopWorker(guildID string) {
-	iw, ok := ttsWorkers.Load(guildID)
-	if !ok {
-		log.Print("no worker for guild ", guildID)
-		return
-	}
-	w := iw.(*worker)
-	w.done <- true
-	close(w.queue)
-	for t := range w.queue {
-		log.Print("task is discorded because worker is stopped: ", t.Text)
-	}
-	ttsWorkers.Delete(guildID)
-}
-
-func do(t TTSTask) error {
-	time.Sleep(200 * time.Millisecond)
-	return discord.Play(t.Text, t.Lang, t.VoiceToken, t.GuildID)
-}
-
-// CleanerWorker starts worker which clean up workers which is alone on voice channels
-func CleanerWorker() {
+func (c *Consumer) Start(ctx context.Context, wg *sync.WaitGroup) {
+L:
 	for {
-		time.Sleep(10 * time.Second)
-		gIDs := []string{}
-		ttsWorkers.Range(func(k interface{}, v interface{}) bool {
-			gID := k.(string)
-			gIDs = append(gIDs, gID)
-			return true
-		})
-		for _, gID := range gIDs {
-			time.Sleep(1 * time.Second)
-			if discord.Alone(gID) {
-				log.Printf("bot is alone in voice channel on guild %s, leave", gID)
-				StopWorker(gID)
-				_ = discord.LeaveVC(gID)
-			}
+		select {
+		case <-ctx.Done():
+			break L
+		default:
 		}
+
+		task, ok := <-c.queue
+		if !ok {
+			log.Printf("task queue of consumer %s is closed", c.ID)
+			break L
+		}
+		log.Print("<-- task received: ", task.ID)
+		if err := task.Do(); err != nil {
+			log.Print("error occurs: ", err)
+		}
+		log.Print("task finished: ", task.ID)
+	}
+
+	log.Printf("stop consumer %s. %d tasks remains", c.ID, len(c.queue))
+	close(c.queue)
+	for task := range c.queue {
+		log.Printf("task %s is discarded. consumer will be killed", task.ID)
+	}
+
+	log.Printf("consume %s is killed", c.ID)
+	wg.Done()
+}
+
+func (c *Consumer) Add(t Task) {
+	select {
+	case c.queue <- t:
+		log.Printf("--> added task %+v to consumer %s", t, c.ID)
+	default:
+		log.Printf("--x discarded task %+v of consumer %s. task queue is full", t, c.ID)
 	}
 }
